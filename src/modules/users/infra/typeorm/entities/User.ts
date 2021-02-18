bd59319b8ee0fb2187560f6852d040bf5c954c0e
matzet99 <matheus.zeitune.developer@gmail.com>
2021-02-18 10:32:07 -0300

    migrations started

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,66 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import uploadConfig from '@config/upload';
+
+import { Exclude, Expose } from 'class-transformer';
+import Address from './Address';
+
+Entity('users');
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  identity: string;
+
+  @Column()
+  birthDate: Date;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  image: string;
+
+  @OneToOne(() => Address)
+  @JoinColumn()
+  address: Address;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Expose({ name: 'image_url' })
+  getAvatarUrl(): string | null {
+    if (!this.image) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.image}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.image}`;
+      default:
+        return null;
+    }
+  }
+}
+
+export default User;
